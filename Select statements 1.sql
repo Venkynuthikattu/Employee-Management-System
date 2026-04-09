75d7c3b82091af107bc1d8eb53f12a426cab62c4
Sai Venkatesh Kumar Nuthikattu <164328589+Venkynuthikattu@users.noreply.github.com>
2026-04-09 20:59:32 +0530

    Create Select statements 1.sql

---
+++ Select statements 1.sql
@@ -0,0 +1,14 @@
+SELECT * FROM employee_demographics;
+SELECT * FROM employee_salary;
+SELECT * FROM parks_departments;
+
+SELECT first_name,
+last_name,
+birth_date
+FROM employee_demographics;
+
+SELECT DISTINCT gender FROM employee_demographics;
+
+SELECT first_name,
+occupation,
+salary from employee_salary;
